be17f1013793ab499af79f413d1797fd339b88bd
AnnaBrum <annahelenabrumark@gmail.com>
2023-08-23 13:40:38 +0200

    styling navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className = " flex flex-wrap justify-between p-5 bg-white">
+      <nav className=" flex flex-wrap justify-between p-5 bg-white">
         <div>
-          <span className = "text-red-600">SAMMANSLAGET</span>
-          
+          <span className="text-red-600">SAMMANSLAGET</span>
         </div>
-        <div className = "flex space-x-2">
-        <Link className = "text-black"href="/">HOME</Link>
-        <Link href="/about" className = "text-black">ABOUT</Link>
+        <div className="flex space-x-2">
+          <Link className="text-black" href="/">
+            HOME
+          </Link>
+          <Link href="/about" className="text-black [font-variation-settings:'wght'_100, 'color'_red] hover:[font-variation-settings:'wght'_600] transition-[font-variation-settings] duration-200">
+            ABOUT
+          </Link>
         </div>
       </nav>
     </>
