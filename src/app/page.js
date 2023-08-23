e6757e7c9601ca4fcae403a15f3202adfd111815
hedlundemma <emmahedlund93@gmail.com>
2023-08-23 11:01:43 +0200

    colors

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,9 @@ export default function Home() {
       <div className = "bg-red-600 h-screen min-h-full">
       <h1 className = "text-9xl p-3 text-white">Urvalsprov Webbutveckling </h1>
       </div>
+      <div className>
+        
+      </div>
     </main>
   );
 }
