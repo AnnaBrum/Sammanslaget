1572e180c902aa076347e62f59194c6388db0435
hedlundemma <emmahedlund93@gmail.com>
2023-08-23 13:44:24 +0200

    push

--- src/app/components/imageCarousel.jsx
+++ src/app/components/imageCarousel.jsx
@@ -13,7 +13,7 @@ const ImageCarousel = ({ images }) => {
   };
 
   return (
-    <div className="relative w-full h-full p-8">
+    <div className="relative w-full h-full p-20">
       <img
         src={images[currentIndex]}
         alt="Carousel Image"
@@ -21,7 +21,7 @@ const ImageCarousel = ({ images }) => {
       />
       
       
-      <div className="absolute bottom-2 left-2 right-2 flex justify-between">
+      <div className="absolute bottom-1 left-12 right-12 flex justify-between">
         <button
           onClick={prevImage}
           className="inline-block text-sm mt-8 border rounded-full tracking-wide border-blue-900 text-white bg-blue-900 px-6 py-3 hover:bg-white hover:text-blue-900 transition"
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
 import ImageCarousel from "./components/imageCarousel";
 
 const images = [
-  "https://plus.unsplash.com/premium_photo-1680553492268-516537c44d91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-  "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  "/Startsida-lightmode.jpeg",
   // Add more image URLs here
 ];
 export default function Home() {
@@ -16,7 +15,9 @@ export default function Home() {
           
         </div>
       </div>
+      
       <div className="flex justify-center items-center h-screen">
+        
         <ImageCarousel images={images} />
       </div>
     </main>
