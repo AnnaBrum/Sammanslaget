33ac3ee8dccf441dbc82d483c91f8c04341eb610
AnnaBrum <annahelenabrumark@gmail.com>
2023-08-23 09:52:57 +0200

    fixed dark/light mode

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HOME</h1>
+    <main className="flex min-h-screen flex-col items-center">
+      <h1>SAMMANSLAGET</h1>
+      <h2>URVALSPROV</h2>
     </main>
   );
 }
